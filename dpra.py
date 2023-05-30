5563ed8dc0d0bb348cb15f1b5af212f4645142c3
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-05-30 14:22:54 +0200

    Update dpra.py

--- dpra.py
+++ dpra.py
@@ -1,7 +1,10 @@
 import psutil
 import socket
 import time
+
 def monitor_outgoing_connections():
+    counter = 0
+    log_messages = []
     while True:
         connections = psutil.net_connections(kind='tcp')
         for conn in connections:
@@ -14,18 +17,24 @@ def monitor_outgoing_connections():
                 log_message = f"Outgoing connection detected:\nProcess: {process_name}\nLocal Address: {local_address}\nRemote Address: {remote_address}\n"
                 print(log_message)
 
-                # You can write the log message to a file for further analysis
-                # Write the log message to a .txt file
-                with open("log.txt", "a") as txt_file:
-                    txt_file.write(log_message + "\n")
+                # Add the log message to the list
+                log_messages.append(log_message)
 
-                # Write the log message to a .csv file
-                with open("log.csv", "a") as csv_file:
-                    csv_file.write(log_message.replace('\n', ',') + "\n")
-                # or both ;)
+                counter += 1
+                if counter >= 60:  # adjust this value as needed
+                    # Write the log messages to a .txt file
+                    with open("log.txt", "a") as txt_file:
+                        txt_file.write('\n'.join(log_messages) + "\n")
+
+                    # Write the log messages to a .csv file
+                    with open("log.csv", "a") as csv_file:
+                        csv_file.write(','.join(log_messages).replace('\n', ',') + "\n")
+
+                    # Reset the counter and the list
+                    counter = 0
+                    log_messages = []
 
         time.sleep(1)
 
 # Start monitoring outgoing connections
 monitor_outgoing_connections()
-
