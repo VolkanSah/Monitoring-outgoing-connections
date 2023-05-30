ffb3900b554214f570a57fd6fac01653b063cb15
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-05-30 14:31:12 +0200

    Update moc.py

--- moc.py
+++ moc.py
@@ -12,29 +12,22 @@ def monitor_outgoing_connections():
                 remote_address = f"{conn.raddr[0]}:{conn.raddr[1]}"
                 local_address = f"{conn.laddr[0]}:{conn.laddr[1]}"
                 process_name = psutil.Process(conn.pid).name()
-
                 # Log the outgoing connection
                 log_message = f"Outgoing connection detected:\nProcess: {process_name}\nLocal Address: {local_address}\nRemote Address: {remote_address}\n"
                 print(log_message)
-
                 # Add the log message to the list
                 log_messages.append(log_message)
-
                 counter += 1
                 if counter >= 60:  # adjust this value as needed
                     # Write the log messages to a .txt file
                     with open("log.txt", "a") as txt_file:
                         txt_file.write('\n'.join(log_messages) + "\n")
-
                     # Write the log messages to a .csv file
                     with open("log.csv", "a") as csv_file:
                         csv_file.write(','.join(log_messages).replace('\n', ',') + "\n")
-
                     # Reset the counter and the list
                     counter = 0
                     log_messages = []
-
         time.sleep(1)
-
 # Start monitoring outgoing connections
 monitor_outgoing_connections()
